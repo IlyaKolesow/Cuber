f5bc7931fea91537d4cc7feaf584afa88203bbe3
Ilya Kolesov <kolesovilya3@gmail.com>
2023-07-20 13:26:15 +0300

    fix: correct display of the add button

--- Backend/delete-from-cart.php
+++ Backend/delete-from-cart.php
@@ -2,5 +2,5 @@
 include('connect.php');
 $delete = "DELETE FROM cart WHERE item_id = $_GET[delete_id] AND user_id = $_SESSION[user_id]";
 mysqli_query($cuber, $delete);
-$_SESSION[$item] = false;
+$_SESSION["item" . $_GET['delete_id']] = false;
 header("Location: ../Cart/cart.php");
